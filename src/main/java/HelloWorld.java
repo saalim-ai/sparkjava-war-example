2a692a20999a26c49e17c08a632dfdd8ced51be9
saalim-ai <57727194+saalim-ai@users.noreply.github.com>
2019-12-24 18:13:38 -0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World");
+		get("/hello/world", (req, res) -> "Hello World, Hello Saalim!");
 	}
 }
